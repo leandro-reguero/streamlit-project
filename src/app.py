140a35463ffbc6bf7fcd247ead5eb34536694d48
leandro-reguero <lreguerolazaro@gmail.com>
2024-08-03 20:36:49 +0200

    appy lista

--- src/app.py
+++ src/app.py
@@ -1,13 +1,29 @@
 from pickle import load
+import numpy as np
+import pandas as pd
 import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+# accediendo a los datos
 model = load(open("models/rfc_default_21.sav", "rb"))
-tfidf = load(open("models/tfidf_vectorizer.sav", "rb"))
+@st.cache_data(ttl= 60*5, max_entries=20)
+def load_data():
+    df = pd.read_csv("data/processed/balanced_data.csv")
+    return df
 
-with open("models/tfidf_vectorizer.sav", "rb") as f:
-    tfidf = load(f)
+df = load_data()
 
+# vectorizador:
+def word(password):
+    character=[]
+    for i in password:
+        character.append(i)
+    return character
+
+X = np.array(df["password"])
+y = np.array(df["strength"])
+tfidf = TfidfVectorizer(tokenizer=word, lowercase=False, token_pattern=None)
+X = tfidf.fit_transform(X)
 
 class_dict = {
     "0": "Weak",
